c1466b7efac2dec105690094b26e2fb63cf38883
Ethan Cheng <ethan@nijika.org>
2024-01-22 12:10:26 -0800

    change accessor methods to const

--- Map.h
+++ Map.h
@@ -6,9 +6,9 @@ class Map
   public:
     Map();         // Create an empty map (i.e., one whose size() is 0).
 
-    bool empty();  // Return true if the map is empty, otherwise false.
+    bool empty() const;  // Return true if the map is empty, otherwise false.
 
-    int size();    // Return the number of key/value pairs in the map.
+    int size() const;    // Return the number of key/value pairs in the map.
 
     bool insert(const std::string& key, const double& value);
       // If key is not equal to any key currently in the map and if the
@@ -38,17 +38,17 @@ class Map
       // pair with that key from the map and return true.  Otherwise, make
       // no change to the map and return false.
      
-    bool contains(const std::string& key);
+    bool contains(const std::string& key) const;
       // Return true if key is equal to a key currently in the map, otherwise
       // false.
      
-    bool get(const std::string& key, double& value);
+    bool get(const std::string& key, double& value) const;
       // If key is equal to a key currently in the map, set value to the
       // value in the map which the key maps to, and return true.  Otherwise,
       // make no change to the value parameter of this function and return
       // false.
      
-    bool get(int i, std::string& key, double& value);
+    bool get(int i, std::string& key, double& value) const;
       // If 0 <= i < size(), copy into the key and value parameters the
       // key and value of the key/value pair in the map whose key is strictly
       // greater than exactly i keys in the map and return true.  Otherwise,
